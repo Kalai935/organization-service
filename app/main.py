2b6406d401d21d403e82e11d54f6ee1c73b6e329
Kalai935 <kalaimuthu098@gmail.com>
2025-12-12 12:53:44 +0530

    Online Deployment

--- app/main.py
+++ app/main.py
@@ -15,6 +15,3 @@ async def shutdown():
 app.include_router(auth.router)
 app.include_router(organization.router)
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
