44dc949b77fbcc8ae9f7a4a30446daba44c5b505
Kalai935 <kalaimuthu098@gmail.com>
2025-12-12 13:56:11 +0530

    Add home page route

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.database import db
 from app.routers import organization, auth
+from fastapi.responses import RedirectResponse
 
 app = FastAPI(title="Org Management Service")
 
@@ -15,6 +16,11 @@ async def shutdown():
 @app.get("/")
 async def read_root():
     return {"message": "Server is up and running!"}
+
+@app.get("/", include_in_schema=False)
+async def root():
+    return RedirectResponse(url="/docs")
+
 
 app.include_router(auth.router)
 app.include_router(organization.router)
